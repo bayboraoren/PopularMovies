317ad8ede59ea533ba79ac0f538bda1e811f00de
baybora <baybora.oren@gmail.com>
2015-09-01 23:37:10 +0300

    https://github.com/bayboraoren/PopularMovies/issues/3

--- app/src/main/java/com/popularmovies/service/MovieRestService.java
+++ app/src/main/java/com/popularmovies/service/MovieRestService.java
@@ -4,7 +4,6 @@ import com.popularmovies.domain.Movies;
 
 import retrofit.http.GET;
 import retrofit.http.Query;
-import retrofit.Callback;
 
 /**
  * Created by bora on 01.09.2015.
@@ -14,11 +13,9 @@ public interface MovieRestService {
     String SORT_BY = "sort_by";
     String API_KEY = "api_key";
 
-
-    @GET("/discover/movie?api_key=c8d8be2cdf2ddd5cb31cf3a04460210c")
-    Movies popular(@Query("sortby") String sortOrder, Callback<Movies> callback);
-
     @GET("/discover/movie")
-    Movies topRated(@Query(API_KEY) String apiKey,@Query(SORT_BY) String sortBy);
+    Movies getMovies(@Query(API_KEY) String apiKey,
+                     @Query(SORT_BY) String sortOrder);
+
 
 }
--- app/src/main/java/com/popularmovies/utility/MovieUtility.java
+++ app/src/main/java/com/popularmovies/utility/MovieUtility.java
@@ -16,10 +16,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import retrofit.Callback;
 import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 /**
  * Created by baybora on 8/27/15.
@@ -32,17 +29,16 @@ public class MovieUtility {
 
         String urlDisplay = "http://image.tmdb.org/t/p/w500" +
                 "/" + posterPath;
-        Bitmap moviePoster = null;
         try {
             InputStream in = new java.net.URL(urlDisplay).openStream();
-            moviePoster = BitmapFactory.decodeStream(in);
+            return BitmapFactory.decodeStream(in);
         } catch (Exception e) {
             Log.e(LOG_TAG, e.getMessage());
+            return null;
         }
-        return moviePoster;
     }
 
-    public static final Movies getMoviesWithRetrofit(Context context) {
+    public static final Movies getMovies(Context context) {
 
         final String MOVIE_DATABASE_BASE_URL =
                 "http://api.themoviedb.org/3";
@@ -55,102 +51,7 @@ public class MovieUtility {
         RestAdapter retrofit = new RestAdapter.Builder().setEndpoint(MOVIE_DATABASE_BASE_URL).build();
         MovieRestService service = retrofit.create(MovieRestService.class);
 
-        Movies movies = service.popular(sortBy, new Callback<Movies>() {
-
-            @Override
-            public void success(Movies movies, Response response) {
-                Log.i("Test", "Test");
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Log.i("Test", "Test");
-            }
-        });
-
-        return null;
-    }
-
-    public static final Movies getMovies(Context context) {
-        // These two need to be declared outside the try/catch
-        // so that they can be closed in the finally block.
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-
-        // Will contain the raw JSON response as a string.
-        String forecastJsonStr = null;
-
-        String sortBy = CommonUtility.getPreferredSortBy(context);
-
-        String sortOrder = sortBy;
-        String apiKey = "c8d8be2cdf2ddd5cb31cf3a04460210c";
-
-        try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are avaiable at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
-            final String FORECAST_BASE_URL =
-                    "http://api.themoviedb.org/3/discover/movie?";
-
-            final String SORT_BY = "sort_by";
-            final String API_KEY = "api_key";
-
-            Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                    .appendQueryParameter(SORT_BY, sortOrder)
-                    .appendQueryParameter(API_KEY, apiKey)
-                    .build();
-
-            URL url = new URL(builtUri.toString());
-
-            // Create the request to OpenWeatherMap, and open the connection
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            // Read the input stream into a String
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null) {
-                // Nothing to do.
-                return null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                buffer.append(line + "\n");
-            }
-
-            if (buffer.length() == 0) {
-                // Stream was empty.  No point in parsing.
-                return null;
-            }
-            forecastJsonStr = buffer.toString();
-            Movies movies = JsonUtility.jsonToMovies(forecastJsonStr);
-
-            return movies;
-
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Error ", e);
-            // If the code didn't successfully get the weather data, there's no point in attempting
-            // to parse it.
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e(LOG_TAG, "Error closing stream", e);
-                }
-            }
-        }
-
-        return null;
+        return service.getMovies(apiKey, sortBy);
 
     }
 
