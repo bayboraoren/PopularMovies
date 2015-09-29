c3ebc5f8d55db0d432416e8e389300e4e46ba0cb
bora <baybora.oren@ucuzcu.com>
2015-09-29 17:14:47 +0300

    https://github.com/bayboraoren/PopularMovies/issues/11

--- app/src/main/java/com/popularmovies/component/MovieDetailAdapter.java
+++ app/src/main/java/com/popularmovies/component/MovieDetailAdapter.java
@@ -19,6 +19,11 @@ public class MovieDetailAdapter extends ArrayAdapter<Trailer> {
 
     private Context context;
 
+    //View Holder
+    static class ViewHolder {
+        MovieTrailerThumbnailImageView movieTrailerThumbnailImageView;
+    }
+
     public MovieDetailAdapter(Context context, ArrayList<Trailer> movieDetail) {
         super(context, 0, movieDetail);
         this.context = context;
@@ -27,26 +32,40 @@ public class MovieDetailAdapter extends ArrayAdapter<Trailer> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-
+        ViewHolder viewHolder;
 
         Trailer trailer = getItem(position);
 
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
+
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.trailer_list_item, parent, false);
+
+            //View Holder
+            viewHolder = new ViewHolder();
+            viewHolder.movieTrailerThumbnailImageView = (MovieTrailerThumbnailImageView)convertView.findViewById(R.id.movie_trailer);
+
+            convertView.setTag(viewHolder);
+            setImage(viewHolder, trailer.getKey());
+
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
+            setImage(viewHolder,trailer.getKey());
         }
 
-        MovieTrailerThumbnailImageView trailerThumbnail = (MovieTrailerThumbnailImageView)convertView.findViewById(R.id.movie_trailer);
 
-        final String youtubeThumbnailUrl = String.format("http://img.youtube.com/vi/%s/mqdefault.jpg",trailer.getKey());
+        return convertView;
+    }
+
+    private void setImage(ViewHolder viewHolder,String trailerKey){
+
+        final String youtubeThumbnailUrl = String.format("http://img.youtube.com/vi/%s/mqdefault.jpg",trailerKey);
 
         Picasso.with(context) //
                 .load(youtubeThumbnailUrl) //
                 .placeholder(R.drawable.placeholder_400_200) //
-                .into(trailerThumbnail);
+                .into(viewHolder.movieTrailerThumbnailImageView);
 
-
-        return convertView;
     }
 
 }
